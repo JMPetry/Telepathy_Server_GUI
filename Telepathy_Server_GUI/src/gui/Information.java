3f7235281e730f8c69f88d0f640518eae8c1527a
Jean Petry <jean-petry@freenet.de>
2018-02-28 19:27:46 +0100

    Add Information scene

--- Telepathy_Server_GUI/src/gui/Information.java
+++ Telepathy_Server_GUI/src/gui/Information.java
@@ -1,9 +1,12 @@
 package gui;
 
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class Information implements SwitchableScene {
@@ -20,14 +23,40 @@ public class Information implements SwitchableScene {
 		VBox vBTopMenu = new VBox();
 		
 		
+		VBox vBInfoContainer = new VBox();
+		vBInfoContainer.setSpacing(10);
+		vBInfoContainer.setAlignment(Pos.CENTER);
 		
+		HBox hBVersionContainer = new HBox();
+		hBVersionContainer.setSpacing(10);
+		hBVersionContainer.setAlignment(Pos.CENTER);
 		
+		Label lVersion = new Label();
+		lVersion.setId("InfoFirstValue");
+		lVersion.setText("Version: ");
 		
+		Label lVersionNumber = new Label();
+		lVersionNumber.setId("lVersionNumber");
+		lVersionNumber.setText(WindowSettings.VERSION_NUMBER);
 		
-		scene = new Scene(root, WindowSettings.WINDOW_WIDTH, WindowSettings.WINDOW_HEIGHT);
+		HBox hBCreatorContainer = new HBox();
+		hBCreatorContainer.setSpacing(10);
+		hBCreatorContainer.setAlignment(Pos.CENTER);
 		
+		Label lCreator = new Label();
+		lCreator.setId("InfoFirstValue");
+		lCreator.setText("Creator: ");
+		
+		Label lCreatorName = new Label();
+		lCreatorName.setId("lCreatorName");
+		lCreatorName.setText("Jean Petry");
+		
+		hBCreatorContainer.getChildren().addAll(lCreator, lCreatorName);
+		hBVersionContainer.getChildren().addAll(lVersion, lVersionNumber);
+		vBInfoContainer.getChildren().addAll(hBVersionContainer, hBCreatorContainer);
 		vBTopMenu.getChildren().addAll(new TopBar(), new NavigationBar());
 		root.setTop(vBTopMenu);
+		root.setCenter(vBInfoContainer);
 		
 		// Make undecorated window dragable
 		root.setOnMousePressed(new EventHandler<MouseEvent>() {
@@ -46,6 +75,7 @@ public class Information implements SwitchableScene {
 			}
 		});
 
+		scene = new Scene(root, WindowSettings.WINDOW_WIDTH, WindowSettings.WINDOW_HEIGHT);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 	}
 
